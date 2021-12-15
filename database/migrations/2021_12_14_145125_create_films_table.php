23bfcf1e5e89b31c565bc75efc3fc45d2290b068
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2021-12-15 10:35:36 +0700

    Memperbaiki halaman login dan register

---
+++ database/migrations/2021_12_14_145125_create_films_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->string('backdrop_path')->nullable();
+            $table->string('poster_path');
+            $table->string('genre');
+            $table->text('overview');
+            $table->string('popularity');
+            $table->date('release_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
